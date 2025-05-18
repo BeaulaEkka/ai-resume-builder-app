ee2b3862add8c795927423d52c9149efc834d5bd
Your Name <ekka.beaula@gmail.com>
2025-05-18 09:52:08 +0200

    feat: implement SkillsForm with validation and debounced updates for resume skills

--- src/app/(main)/editor/forms/SkillsForm.tsx
+++ src/app/(main)/editor/forms/SkillsForm.tsx
@@ -1,8 +1,18 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { EditorFormProps } from "@/lib/types";
 import { skillsSchema, SkillsValues } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Resolver } from "dns";
-import React from "react";
+import debounce from "lodash.debounce";
+
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 export default function SkillsForm({
@@ -11,6 +21,67 @@ export default function SkillsForm({
 }: EditorFormProps) {
   const form = useForm<SkillsValues>({
     resolver: zodResolver(skillsSchema),
+    defaultValues: {
+      skills: resumeData.skills || [],
+    },
   });
-  return <div>skills</div>;
+
+  useEffect(() => {
+    const debouncedValidateAndUpdate = debounce(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      // update the resume preview
+      setResumeData({
+        ...resumeData,
+        skills:
+          values.skills
+            ?.filter((skill) => skill !== undefined)
+            .map((skill) => skill.trim())
+            .filter((skill) => skill !== "") || [],
+      });
+    }, 500);
+
+    const subscription = form.watch((values) => {
+      debouncedValidateAndUpdate(values);
+    });
+
+    return () => {
+      subscription.unsubscribe(); // cleanup
+      debouncedValidateAndUpdate.cancel(); // cancel any pending calls
+    };
+  }, [form, resumeData, setResumeData]);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="spaye-y-1.5 text-center">
+        <h2 className="text-2xl font-bold">Skills</h2>
+        <p className="text-muted-foreground text-sm">
+          Tell us about your Skills. This will appear on your resume.
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-8">
+          <FormField
+            control={form.control}
+            name="skills"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Skills</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your skills"
+                    value={field.value}
+                    onChange={(e) => {
+                      field.onChange(e.target.value);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 }
