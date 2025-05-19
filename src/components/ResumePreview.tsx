c6cf159e575b6caf6ba4e0d36be0b07ec7c2c428
Your Name <ekka.beaula@gmail.com>
2025-05-19 23:48:54 +0200

    feat: update ResumePreview component to include profile image and adjust header structure

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validations";
+import Image from "next/image";
 
 export interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -15,7 +16,7 @@ export default function ResumePreview({
       className={cn("aspect-[210/297] w-full bg-white text-black", className)}
     >
       <h1 className="p-6 text-3xl font-bold">
-        This text should change the size of the container div
+        {/* This text should change the size of the container div */}
       </h1>
       <div>
         <PersonalInfoHeader resumeData={resumeData} />
@@ -31,7 +32,9 @@ interface ResumeSectionProps {
 function PersonalInfoHeader({ resumeData }: ResumeSectionProps) {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-3xl font-bold">{resumeData.title}</h1>
+      <Image href={resumeData.photoUrl} alt="profile image"></Image>
+      <h1 className="text-3xl font-bold">{resumeData.firstName}</h1>
+      <h1 className="text-3xl font-bold">{resumeData.lastName}</h1>
       <p className="text-lg">{resumeData.description}</p>
     </div>
   );
