8176a13423f94fa92a511dfed96f04c88aec1923
Your Name <ekka.beaula@gmail.com>
2025-05-20 15:41:22 +0200

    feat: enhance PersonalInfoHeader to handle profile image preview and state management

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validations";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -30,9 +31,37 @@ interface ResumeSectionProps {
 }
 
 function PersonalInfoHeader({ resumeData }: ResumeSectionProps) {
+  const {
+    firstName,
+    lastName,
+    description,
+    photo,
+    email,
+    phone,
+    city,
+    country,
+    zipCode,
+  } = resumeData;
+
+  //photot is string
+  const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? "" : photo);
+
+  //convert file into string
+  useEffect(() => {
+    const objectUrl =
+      photo instanceof File ? URL.createObjectURL(photo) : photo;
+    if (objectUrl) {
+      setPhotoSrc(objectUrl);
+      if (photo === null) {
+        setPhotoSrc(""); //to remove the preview
+        return()=> URL.revokeObjectURL(objectUrl);
+      }
+    }
+  }, [photo]);
+
   return (
     <div className="flex flex-col gap-2">
-      <Image href={resumeData.photoUrl} alt="profile image"></Image>
+      <Image src="photoSrc" alt="profile image" width={250} height={250} />
       <h1 className="text-3xl font-bold">{resumeData.firstName}</h1>
       <h1 className="text-3xl font-bold">{resumeData.lastName}</h1>
       <p className="text-lg">{resumeData.description}</p>
